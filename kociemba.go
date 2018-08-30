7da0c57fde886d916f7c2816aa8152c8e9201a8a
daosyn <daosyn@gmail.com>
2018-08-29 20:47:47 -0500

    add main file describing steps

---
+++ kociemba.go
@@ -0,0 +1,14 @@
+// divides implementation into two steps
+
+package kociemba
+
+// phase 1:
+// simplify scrambled cube into a state such
+// that U and D consist of oriented pieces
+func reduce() {
+}
+
+// phase 2:
+// solve the reduced cube from phase 1
+func complete() {
+}
