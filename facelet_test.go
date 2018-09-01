6dbd5634584b371250a32bede65372291cca5530
daosyn <daosyn@gmail.com>
2018-09-01 14:24:06 -0500

    configure proper turn arrays

--- facelet_test.go
+++ facelet_test.go
@@ -1,7 +1,11 @@
 package kociemba
 
 import "testing"
+import "fmt"
 
 func TestPermuteFace(t *testing.T) {
-
+	fmt.Println(permuteFace(faceMove[U], U))
+	fmt.Println(permuteFace(faceMove[R], D))
+	fmt.Println(permuteFace(faceMove[B], L))
+	fmt.Println(permuteFace(faceMove[F], B))
 }
