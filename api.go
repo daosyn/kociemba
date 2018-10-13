131b754e5c85dc43f12140eb72af496e38d19353
daosyn <daosyn@gmail.com>
2018-10-12 20:07:17 -0500

    refactor NewScramble() to Get... naked return scramble+layout

--- api.go
+++ api.go
@@ -7,31 +7,31 @@ import (
 	"math/rand"
 )
 
-// NewScramble returns a random state of the Rubik's Cube.
-func NewScramble() []string {
+// GetNewScramble returns scramble, the sequence of moves that will transform a solved cube to the returned layout.
+func GetNewScramble() (scramble []string, layout string) {
 	var mod = [3]string{"", "'", "2"}
 	var x = [2]string{"R", "L"}
 	var y = [2]string{"U", "D"}
 	var z = [2]string{"F", "B"}
-	var s []string
 	curr := -1
 	for i := 0; i < 25; i++ {
 		next := rand.Intn(3)
 		if next != curr {
 			switch next {
 			case 0:
-				s = append(s, x[rand.Intn(2)]+mod[rand.Intn(3)])
+				scramble = append(scramble, x[rand.Intn(2)]+mod[rand.Intn(3)])
 			case 1:
-				s = append(s, y[rand.Intn(2)]+mod[rand.Intn(3)])
+				scramble = append(scramble, y[rand.Intn(2)]+mod[rand.Intn(3)])
 			case 2:
-				s = append(s, z[rand.Intn(2)]+mod[rand.Intn(3)])
+				scramble = append(scramble, z[rand.Intn(2)]+mod[rand.Intn(3)])
 			}
 			curr = next
 		} else {
 			i--
 		}
 	}
-	return s
+	layout = "UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB"
+	return
 }
 
 // Solve returns a string of moves that solves the provided scramble.
